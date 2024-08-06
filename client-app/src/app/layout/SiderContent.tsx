08b5cf35b2f230ff315c24a4a15a7a7239838cc9
Alperen Kılıç <alp.kilic.97@gmail.com>
2024-08-06 13:57:34 +0300

    simplebar added

--- client-app/src/app/layout/SiderContent.tsx
+++ client-app/src/app/layout/SiderContent.tsx
@@ -2,6 +2,8 @@ import { Avatar, Button, Menu, Typography } from "antd";
 import { items } from "../utils/MenuItems";
 import { useStore } from "../stores/store";
 import { LogoutOutlined } from "@ant-design/icons";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 
 const { Title } = Typography;
 
@@ -13,29 +15,31 @@ const SiderContent = ({ collapseHandler }: Props) => {
   const { userStore } = useStore();
   const { user } = userStore;
   return (
-    <div className="sider">
-      <div className="sider-logo">
-        <img src="/assets/logo-light.png" alt="Beylikdüzü Belediyesi Logo" />
-        <Avatar
-          className="avatar"
-          size={80}
-          src={<img src={"/assets/avatar.jpg"} alt="avatar" />}
-        />
+    <SimpleBar style={{ height: "100%" }}>
+      <div className="sider">
+        <div className="sider-logo">
+          <img src="/assets/logo-light.png" alt="Beylikdüzü Belediyesi Logo" />
+          <Avatar
+            className="avatar"
+            size={80}
+            src={<img src={"/assets/avatar.jpg"} alt="avatar" />}
+          />
+        </div>
+        <Title className="title" level={5}>
+          {user?.email}
+        </Title>
+        <Menu items={items} theme="dark" mode="inline" onClick={() => collapseHandler()} />
+        <Button
+          icon={<LogoutOutlined />}
+          type="link"
+          danger
+          onClick={() => userStore.logout()}
+          style={{ width: "100%", display: "flex", marginLeft: "13px", alignItems: "center" }}
+        >
+          Çıkış Yap
+        </Button>
       </div>
-      <Title className="title" level={5}>
-        {user?.email}
-      </Title>
-      <Menu items={items} theme="dark" mode="inline" onClick={() => collapseHandler()} />
-      <Button
-        icon={<LogoutOutlined />}
-        type="link"
-        danger
-        onClick={() => userStore.logout()}
-        style={{ width: "100%", display: "flex", marginLeft: "13px", alignItems: "center" }}
-      >
-        Çıkış Yap
-      </Button>
-    </div>
+    </SimpleBar>
   );
 };
 
